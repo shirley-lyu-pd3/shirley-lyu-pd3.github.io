2284d34717e019dd3d02a6ab625ceaf586238daf
Haowen Liu <liu.haowen.andy@gmail.com>
2020-11-30 20:35:40 -0800

    Update index.html

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,18 @@
-<a href="scene1">Scene 1</a>
-<a href="scene2">Scene 2</a>
+<h1>Shirley Lyu PD3</h1>
+<h2>Scenes</h2>
+<ul>
+  <li><a href="scene1">Scene 1</a></li>
+  <li><a href="scene2">Scene 2</a></li>
+</ul>
 
-<div>
-  Font made from
-  <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC
-  BY 3.0
-</div>
+<h2>Copyright notice</h2>
+<dl>
+  <dt>Houschka Rounded Medium</dt>
+  <dd>
+    <div>
+      Font made from
+      <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by
+      CC BY 3.0
+    </div>
+  </dd>
+</dl>
