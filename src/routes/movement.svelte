4e1aa0d901141784084d356e9517f0e304408026
Haowen Liu <liu.haowen.andy@gmail.com>
2020-12-16 14:12:56 -0800

    Movement

--- src/routes/movement.svelte
+++ src/routes/movement.svelte
@@ -1,11 +1,23 @@
-<style>
+<style lang="postcss">
   #container {
     position: relative;
     width: 960px;
     height: 1080px;
-    background-image: url("../background2.jpg");
-    background-size: cover;
     overflow: hidden;
+  }
+
+  #background {
+    position: absolute;
+    left: -530px;
+    top: -1080px;
+    width: 1920px;
+    height: 2160px;
+  }
+
+  #background img {
+    object-fit: cover;
+    filter: blur(3px);
+    animation: bg_movement linear 10s 1s forwards;
   }
 
   .window {
@@ -20,7 +32,7 @@
     left: 160px;
     background-image: url("../research.png");
     background-size: cover;
-    animation: research_movement 45s linear 1s infinite;
+    animation: research_movement linear 10s 1s forwards;
   }
 
   #word {
@@ -30,7 +42,7 @@
     background-size: contain;
     background-repeat: no-repeat;
     background-position: right;
-    animation: side_movement 45s linear 1s infinite;
+    animation: side_movement linear 10s 1s forwards;
   }
 
   #music {
@@ -40,35 +52,26 @@
     background-size: contain;
     background-repeat: no-repeat;
     background-position: left;
-    animation: side_movement 45s linear 1s infinite;
+    animation: side_movement linear 10s 1s forwards;
   }
 
   #layout {
-    top: 480px;
+    top: 430px;
     left: 160px;
     background-image: url("../layout.GIF");
     background-size: cover;
-    animation: main_movement 45s linear 1s infinite;
+    @apply ring-4 ring-green ring-opacity-50;
   }
 
   @keyframes research_movement {
     0% {
       transform: translateX(0);
     }
-    8% {
-      transform: translateX(-700px);
+    80% {
+      transform: translateX(-700px) translateY(60px);
     }
-    22% {
-      transform: translateX(-700px);
-    }
-    44% {
-      transform: translateX(-700px);
-    }
-    67% {
-      transform: translateX(0);
-    }
-    78% {
-      transform: translateX(0);
+    100% {
+      transform: translateX(-700px) translateY(120px);
     }
   }
 
@@ -76,40 +79,23 @@
     0% {
       transform: translateY(0);
     }
-    22% {
-      transform: translateY(200px);
-    }
-    44% {
-      transform: translateY(200px);
-    }
-    67% {
-      transform: translateY(-200px);
-    }
-    78% {
-      transform: translateY(-200px);
+    100% {
+      transform: translateY(300px);
     }
   }
 
-  @keyframes main_movement {
+  @keyframes bg_movement {
     0% {
       transform: translateY(0);
     }
-    22% {
-      transform: translateY(-400px);
-    }
-    44% {
-      transform: translateY(-400px);
-    }
-    67% {
-      transform: translateY(200px);
-    }
-    78% {
-      transform: translateY(200px);
+    100% {
+      transform: translateY(350px);
     }
   }
 </style>
 
 <div id="container">
+  <div id="background"><img src="background2.jpg" alt="background" /></div>
   <div class="window" id="research" />
   <div class="window" id="word" />
   <div class="window" id="music" />
